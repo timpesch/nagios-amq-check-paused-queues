2d5d6d87cf1b6da260419f2a0a3bc233cdaf5fe1
timpesch <tp@peschmail.com>
2017-04-21 14:46:49 +0200

    Added support for checking Queuesize

--- check_amq_paused_queues.sh
+++ check_amq_paused_queues.sh
@@ -17,9 +17,16 @@ function print_help()
         echo "              -u       User name and password. REQUIRED OPTION"
         echo "              -q       Name of Queue you want to check. REQUIRED OPTION"
         echo ""
-        echo "              -q       Check if Queue is in paused state"
+        echo "   Pause Check:"
+        echo "              -p       Check if Queue is in paused state"
         echo ""
-        exit $STATE_UNKN
+        echo "   Queuesize Check:"
+        echo "              -s       Checks the Queuesize"
+        echo "              -w       Warn Value"
+        echo "              -c       Crit Value"
+
+
+        echo ""
 }
 
 function check_access()
@@ -45,48 +52,84 @@ function check_paused()
         fi
 }
 
+function check_queuesize()
+{
+        OUT=$(curl -s -u $username "http://$AMQHOST:$HTTPPORT$JOLOKIA,destinationType=Queue,destinationName=$queue/QueueSize")
+
+        # Check Status Code
+        if echo $OUT | grep '"status":200' >/dev/null
+                then
+                        QUEUESIZE=$(echo $OUT | cut -d'}' -f2 | cut -d':' -f2 | cut -d',' -f1)
+
+                        if [ $QUEUESIZE -lt $crit_val ] && [ $QUEUESIZE -lt $warn_val ]
+                                then
+                                        echo "OK - Queuesize $QUEUESIZE"
+                                        exit $STATE_OK
+                        fi
+
+                        if [ $QUEUESIZE -gt $crit_val ]
+                                then
+                                        echo "Crit - Queuesize $QUEUESIZE"
+                                        exit $STATE_CRIT
+                        fi
+
+                        if [ $QUEUESIZE -gt $warn_val ]
+                                then
+                                        echo "Warn - Queuesize $QUEUESIZE"
+                                        exit $STATE_WARN
+                        fi
+
+        fi
+
+
+
+}
+
 
 #########################################################
 ##                      MAIN CODE                      ##
 #########################################################
 
-# Make sure the correct number of command line arguments have been supplied
-if [[ $# -eq 0 ]]; then
-        print_help
-        exit $STATE_UNKN
-fi
+while getopts ":u:q:psw:c:" opt; do
+  case $opt in
 
-# Make sure all necessary arguments were given; EXIT with an UNKNOWN status if not
-while test -n "$1"; do
-    case "$1" in
-        --help)
-            print_help
-            exit $STATE_OK
-            ;;
-        -h)
-            print_help
-            exit $STATE_OK
-            ;;
-        -u)
-            username=$2
-            shift
-            ;;
-        -p)
-            paused_status=true
-            check=true
-            shift
-            ;;
-        -q)
-            queue=$2
-            shift
-            ;;
-        *)
-            echo "Unknown argument: $1"
-            print_help
-            exit $STATE_UNKN
-            ;;
-    esac
-    shift
+        u)
+        username=$OPTARG
+        ;;
+
+        q)
+        queue=$OPTARG
+        ;;
+
+        p)
+        paused_status=true
+        check=true
+        ;;
+
+        s)
+        size_status=true
+        check=true
+        ;;
+
+        w)
+        warn_val=$OPTARG
+        ;;
+
+        c)
+        crit_val=$OPTARG
+        ;;
+
+        \?)
+        echo "Invalid option: -$OPTARG" >&2
+        ;;
+
+        :)
+        print_help
+        echo -e "Option -$OPTARG requires an argument.\n" >&2
+
+        exit $STATE_UNKN
+        ;;
+  esac
 done
 
 
@@ -116,4 +159,17 @@ if [ $paused_status ]
         then
                 check_access
                 check_paused
+fi
+
+if [ $size_status ]
+        then
+                if ! [ -n "$warn_val" ] || ! [ -n "$crit_val" ]
+                        then
+                                print_help
+                                echo "Check Warn and Crit Values"
+                                exit $STATE_UNKN
+                fi
+
+                check_access
+                check_queuesize
 fi
